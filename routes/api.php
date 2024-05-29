b3271a1881396121a2f009867766f50ca4625550
Andrey Kudryavtsev <andykey@bk.ru>
2024-05-29 16:13:33 +0700

    api route

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth_api')->group(
+Route::post('/tokens/create', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::middleware('auth:sanctum')->group(
 	function () {
 		Route::get('/user/{user}', function (User $user) {
 			return  'route get user ' . $user;
